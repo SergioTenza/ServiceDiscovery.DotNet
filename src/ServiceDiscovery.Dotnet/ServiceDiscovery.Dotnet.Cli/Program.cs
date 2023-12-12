fd152aa2fd356e996174b5d2be64c5b7babef91c
Sergio Tenza <sergeix77@gmail.com>
2023-12-12 21:43:50 +0100

    CODING: CLI First Options

--- src/ServiceDiscovery.Dotnet/ServiceDiscovery.Dotnet.Cli/Program.cs
+++ src/ServiceDiscovery.Dotnet/ServiceDiscovery.Dotnet.Cli/Program.cs
@@ -1,11 +1,37 @@
 ﻿using Spectre.Console;
+using StackExchange.Redis;
 
 internal class Program
 {
+
+    static ConnectionMultiplexer RedisConnection;
     public static void Main(string[] args)
     {
-        AnsiConsole.Markup("[underline Green]ServiceDiscovery.Dotnet[/]\r\n");
-        var name = AnsiConsole.Ask<string>("What's your [green]name[/]?");
-        AnsiConsole.WriteLine($"Hi {name} welcome to 'ServiceDiscovery.Dotnet' ");
+        if(args.Length > 0)
+        {
+            //TODO: Pure CLI flow
+        }
+        else
+        {
+            AnsiConsole.Markup("[underline Green]ServiceDiscovery.Dotnet[/]\r\n");
+            var communication = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+            .Title("Which [green]communication pattern[/] would you use?")
+            .PageSize(3)        
+            .AddChoices(new[] {
+                "Rest", "RabbitMQ", "Redis"
+            }));
+            var connection = communication switch 
+            {
+                "Redis" => AnsiConsole.Ask<string>("What's your [green]Redis[/] connectionstring?"),
+                "RabbitMQ" => AnsiConsole.Ask<string>("What's your [green]RabbitMQ[/] connectionstring?"),
+                "Rest" => AnsiConsole.Ask<string>("What's your [green]Rest[/] connectionstring?"),
+                _ => string.Empty
+            };
+
+            AnsiConsole.WriteLine($"Selected {communication} with ConnectionString: '{connection}' ");
+        }
+
+        
     }
 }
