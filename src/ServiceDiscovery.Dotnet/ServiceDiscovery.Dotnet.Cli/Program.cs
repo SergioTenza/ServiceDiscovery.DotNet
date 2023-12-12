37fbce7160e69c53d04df2041e22ce95785b5e94
Sergio Tenza <sergeix77@gmail.com>
2023-12-12 21:26:04 +0100

    INFRA: Added CLI project

---
+++ src/ServiceDiscovery.Dotnet/ServiceDiscovery.Dotnet.Cli/Program.cs
@@ -0,0 +1,11 @@
+﻿using Spectre.Console;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        AnsiConsole.Markup("[underline Green]ServiceDiscovery.Dotnet[/]\r\n");
+        var name = AnsiConsole.Ask<string>("What's your [green]name[/]?");
+        AnsiConsole.WriteLine($"Hi {name} welcome to 'ServiceDiscovery.Dotnet' ");
+    }
+}
