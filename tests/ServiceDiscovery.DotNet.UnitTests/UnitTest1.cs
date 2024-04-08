ba04d3faf6842183fa4a513f791ac2c1d80aa947
Sergio Tenza Lopez <sergeix_tc@hotmail.com>
2024-04-08 18:06:45 +0200

    INFRA: Added UnitTests project #18

---
+++ tests/ServiceDiscovery.DotNet.UnitTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace ServiceDiscovery.DotNet.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void DeleteMe()
+        {
+            Assert.True(true);
+
+        }
+    }
+}
